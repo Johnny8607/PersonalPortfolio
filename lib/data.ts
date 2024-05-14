f4f48237c38147f0f9f388f3e56345a63374b6e0
Johnny8607 <dangjohnny0543@gmail.com>
2024-05-14 15:49:01 -0400

    experience

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,5 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import carRentalImg from "@/public/carRental.png";
 import petAdoptionImg from "@/public/petAdoption.png";
@@ -38,25 +37,17 @@ export const experiencesData = [
     title: "Software Engineering student",
     location: "Concordia University",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+      "Currently pursuing a Bachelor in Software Engineering.",
     icon: React.createElement(LuGraduationCap),
-    date: "2019",
+    date: "2022 - present",
   },
   {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
+    title: "Front-End Developer Intern",
+    location: "Genetec",
     description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "I worked as a front-end developer intern within the UX team for 4 months. I have gained experience with popular frameworks such as React, Angular, and Vue.",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "2023",
   },
 ] as const;
 
